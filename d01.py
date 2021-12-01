237d8ec21944fcc2684b721157626e258aad2664
Kevin Xia <kevinmxia@gmail.com>
2021-12-01 00:09:52 -0500

    Day 1.

---
+++ d01.py
@@ -0,0 +1,24 @@
+def readFile(filename):
+    lines = []
+    with open(filename, 'r') as f:
+        for line in f:
+            lines.append(line[:-1])
+    return lines
+
+
+lines = readFile("d01input.txt")
+vals = []
+for line in lines:
+    vals.append(int(line))
+
+count = 0
+for i in range(3, len(vals)):
+    if vals[i] > vals[i-1]:
+        count += 1
+print(count)
+
+count = 0
+for i in range(3, len(vals)):
+    if sum(vals[i-2:i+1]) > sum(vals[i-3:i]):
+        count += 1
+print(count)
--- graph_util.py
+++ graph_util.py
@@ -21,6 +21,55 @@ def _top_sort_helper(adj_list, v, visited, stack):
     stack.append(v)
 
 
+def transpose(adj_list):
+    new_adj_list = dict()
+    for key in adj_list:
+        new_adj_list[key] = []
+
+    for key in adj_list:
+        for v in adj_list[key]:
+            if v not in new_adj_list:
+                new_adj_list[v] = []
+            new_adj_list[v].append(key)
+    return new_adj_list
+
+
+def scc(adj_list):
+    visited = set()
+    back_order = []
+
+    def visit(u):
+        if u not in visited:
+            visited.add(u)
+            for v in adj_list[u]:
+                visit(v)
+            back_order.append(u)
+
+    for u in adj_list:
+        visit(u)
+
+    visited = set()
+    adj_list_t = transpose(adj_list)
+
+    def assign(u):
+        if u not in visited:
+            visited.add(u)
+            component = [u]
+            for v in adj_list_t[u]:
+                result = assign(v)
+                if result is not None:
+                    component.extend(result)
+            return component
+        return None
+
+    components = []
+    for u in back_order[::-1]:
+        comp = assign(u)
+        if comp is not None:
+            components.append(comp)
+    return components
+
+
 def bfs(adj_list, key, target):
     explored = dict()
     q = [(key, 0)]
@@ -43,4 +92,5 @@ if __name__ == "__main__":
     adj_list['x'] = ['y']
     adj_list['y'] = []
     adj_list['z'] = ['x', 'y']
-    print(top_sort(adj_list))
+
+    print(scc(adj_list))
